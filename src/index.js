5ad96c6a323f828413d87f237c11e7d783f4db70
unknown <amuktamalyadav@gmail.com>
2025-12-09 23:58:17 +0530

    Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const postsRouter = require("./routes/posts");
-
 const app = express();
 
 app.use(express.json());
-
 app.use("/api", postsRouter);
-
 app.get("/", (req, res) => {
   res.send("Global API assignment server is running");
 });
